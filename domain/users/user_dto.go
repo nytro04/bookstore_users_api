799a2ddcd637fe6ce2098496274add57053aa17d
Francis Badasu <nytro04@gmail.com>
2021-07-21 05:41:29 +0000

    feat: add email validation method for user

---
+++ domain/users/user_dto.go
@@ -0,0 +1,24 @@
+package users
+
+import (
+	"strings"
+
+	"github.com/nytro04/bookstore_users_api/utils/errors"
+)
+
+type User struct {
+	Id          int64  `json: "id"`
+	FirstName   string `json: "first_name"`
+	LastName    string `json: "last_name"`
+	Email       string `json: "email"`
+	DateCreated string `json: "date_created"`
+}
+
+func (user *User) Validate() *errors.RestErr {
+	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+
+	if user.Email == "" {
+		return errors.NewBadRequestError("Please provide a valid email address")
+	}
+	return nil
+}
