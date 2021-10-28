4a8eee7eadfa2aa6374244def8523e99ac832fbc
Francis Badasu <nytro04@gmail.com>
2021-10-28 03:14:26 +0000

    feat: add env with viper

---
+++ utils/env_utils/env_utils.go
@@ -0,0 +1,32 @@
+package env_utils
+
+import (
+	"github.com/spf13/viper"
+)
+
+// Config stores all the configuration of the application
+// The values are read by viper from a config file or environment variables
+
+type Config struct {
+	SQLUsername string `mapstructure:"MYSQL_USERNAME"`
+	SQLPassword string `mapstructure:"MYSQL_PASSWORD"`
+	SQLHost     string `mapstructure:"MYSQL_HOST"`
+	SQLSchema   string `mapstructure:"MYSQL_SCHEMA"`
+}
+
+// LoadConfig reads configuration from file or environment variables
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
